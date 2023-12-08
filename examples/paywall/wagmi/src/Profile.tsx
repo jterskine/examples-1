fb7f22e45910b198083517492fff3928e4d20826
Joey Erskine <joeterskine@gmail.com>
2023-12-08 07:57:15 -0800

    Show repeated signatures with metamask when closing modal

--- examples/paywall/wagmi/src/Profile.tsx
+++ examples/paywall/wagmi/src/Profile.tsx
@@ -1,4 +1,4 @@
-import { useAccount, useConnect, useDisconnect } from 'wagmi'
+import { useAccount, useChainId, useConnect, useDisconnect } from 'wagmi'
 import { InjectedConnector } from 'wagmi/connectors/injected'
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore (this is a local package)
@@ -9,6 +9,7 @@ const paywall = new Paywall(networks)
 
 export function Profile() {
   const { address, isConnected, connector } = useAccount()
+  const chainId = useChainId();
   const { connect } = useConnect({
     connector: new InjectedConnector(),
   })
@@ -18,12 +19,14 @@ export function Profile() {
     if (connector) {
       const paywallConfig = {
         locks: {
-          '0x6c208a3eb3150e7896124d282095f9e5fca18780': {
-            network: 5,
+          '0x6E073485c8177b055de1521878c5dDC4F62A65d7': {
+            network: chainId,
           },
         },
+        title: 'Hello world',
+        icon: 'https://i.seadn.io/gcs/files/00c274eb1f4440f3f4919e6d9e40b35d.png?auto=format&dpr=1&w=48',
+        skipSelect: true,
         skipRecipient: true,
-        title: 'My Membership',
       }
       await paywall.connect(await connector.getProvider())
       await paywall.loadCheckoutModal(paywallConfig)
@@ -34,7 +37,7 @@ export function Profile() {
 
   if (isConnected)
     return (
-      <div>
+      <div style={{backgroundColor: 'lavender'}}>
         Connected to {address}
         <button onClick={() => disconnect()}>Disconnect</button>
         <button onClick={() => checkout()}>Checkout!</button>
